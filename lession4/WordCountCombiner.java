c8920ecb5f7eae95175d36b310285ed830ef2fce
vashdev <vashodha@yahoo.com>
2014-11-14 06:06:09 -0500

    small update

--- lession4/WordCountCombiner.java
+++ lession4/WordCountCombiner.java
@@ -20,14 +20,17 @@ import org.apache.hadoop.util.GenericOptionsParser;
                        Context context
                        ) throws IOException, InterruptedException {
 // since the mapper output and reducer outputs are of same type we cna keep the key value types same
-// we will filter keys and eleminate any records that has word Berners-Lee
+// we will filter keys and eleminate any records that has word Berners-Lee or whos total vlaues are less then min value
 
       int sum = 0;
       for (IntWritable val : values) {
         sum += val.get();
       }
       result.set(sum);
-// filter for a value
-      if(key.toString().equals("Berners-Lee")==false) context.write(key, result);
+// access runtime parameter for min value
+Configuration conf = context.getConfiguration();
+int min=Integer.parseInt(conf.get("MINVAL"));
+// filter for a value, 
+      if((key.toString().equals("Berners-Lee")==false) || sum>min ) context.write(key, result);
     }
   }
